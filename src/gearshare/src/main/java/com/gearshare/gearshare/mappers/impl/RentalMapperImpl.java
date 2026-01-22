0148fd1ba54195c4490f184f7c77f5add9636176
Laura Lučin <laura.lucin@fer.hr>
2026-01-22 23:31:29 +0100

    dodan api za iznajmljivanje

--- src/gearshare/src/main/java/com/gearshare/gearshare/mappers/impl/RentalMapperImpl.java
+++ src/gearshare/src/main/java/com/gearshare/gearshare/mappers/impl/RentalMapperImpl.java
@@ -1,12 +1,12 @@
 package com.gearshare.gearshare.mappers.impl;
 
 import com.gearshare.gearshare.domain.dto.RentalDto;
-import com.gearshare.gearshare.domain.dto.RentalDto;
-import com.gearshare.gearshare.domain.entities.RentalEntity;
 import com.gearshare.gearshare.domain.entities.RentalEntity;
 import com.gearshare.gearshare.mappers.Mapper;
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RentalMapperImpl implements Mapper<RentalEntity, RentalDto> {
 
     private final ModelMapper modelMapper;
