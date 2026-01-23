40a87c00dd77df2ca1a49f00c27c979110564eab
Matija Antun Dürrigl <47604196+digi235@users.noreply.github.com>
2026-01-23 22:08:51 +0100

    Update ImageController.java

--- src/gearshare/src/main/java/com/gearshare/gearshare/controllers/ImageController.java
+++ src/gearshare/src/main/java/com/gearshare/gearshare/controllers/ImageController.java
@@ -13,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.io.IOException;
 import java.util.List;
